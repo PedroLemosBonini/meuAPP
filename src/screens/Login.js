36b4687abebc3d557bff301355aeadffc8932836
pedrolemosbonini <pedrolemosbonini@gmail.com>
2025-03-26 07:58:42 -0300

    Olho Senha

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,11 +9,13 @@ import {
   Button,
 } from "react-native";
 import api from "../axios/axios";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Login({ navigation }) {
   const [user, setUser] = useState({
     email: "",
     password: "",
+    showPassword: false,
   });
 
   async function handleLogin() {
@@ -21,67 +23,95 @@ export default function Login({ navigation }) {
       (response) => {
         console.log(response.data.message);
         Alert.alert(response.data.message);
-        navigation.navigate("Home")
+        navigation.navigate("Home");
       },
       (error) => {
-        Alert.alert(error.response.data.error);
         console.log(error);
+        Alert.alert("Erro", error.response.data.error);
       }
     );
   }
 
   return (
-    
-      <View style={styles.container}>
-        <Text style={styles.title}>Faça Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Faça Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={user.email}
+        onChangeText={(value) => {
+          setUser({ ...user, email: value });
+        }}
+      />
+
+      <View style={styles.passwordContainer}>
         <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={user.email}
-          onChangeText={(value) => {
-            setUser({ ...user, email: value });
-          }}
-        />
-        <TextInput
-          style={styles.input}
+          style={styles.passwordInput}
           placeholder="Senha"
           value={user.password}
+          secureTextEntry={user.showPassword}
           onChangeText={(value) => {
             setUser({ ...user, password: value });
           }}
         />
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text>Entrar</Text>
+
+        <TouchableOpacity
+          onPress={() => setUser({ ...user, showPassword: !user.showPassword })}
+        >
+          <Ionicons
+            name={user.showPassword ? "eye-off" : "eye"}
+            size={24}
+            color={"gray"}
+          />
         </TouchableOpacity>
-        <Button
-        title="Cadastro"
-        onPress={()=> navigation.navigate("Cadastro")}
-        />
       </View>
-    
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+
+      <Button
+        title="Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 28,
     fontWeight: "bold",
   },
   input: {
+    borderColor: "gray",
     width: "100%",
     height: 40,
     borderBottomWidth: 1,
     marginBottom: 20,
-    paddingHorizontal: 10, // Corrigido aqui
+    paddingHorizontal: 10,
   },
   button: {
-    backgroundColor: "green",
+    margin: 5,
+    backgroundColor: "#00A6FF",
     padding: 10,
-    borderRadius: 5,
+    boderRadius: 5,
+  },
+
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    paddingRight: 10,
+    borderBottomWidth:1
+  },
+  passwordInput: {
+    flex: 1,
+    height: 40,
   },
 });
